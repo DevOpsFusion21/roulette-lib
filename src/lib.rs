3afabfc3a1aebf5471760ca13be7deb4d91bbebd
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-09-13 20:39:25 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod tests {
+
+    pub fn fib(n: u32) -> u64 {
+        match n {
+            0 => panic!("Zero is not a valid argument"),
+            1 | 2 => 1,
+            _ => fib(n - 1) + fib(n - 2)
+        }
+    }
+ 
+    #[test]
+    fn fib_1() {
+        assert_eq!(fib(1), 1);
+    }
+
+    #[test]
+    fn fib_2() {
+        assert_eq!(fib(2), 1);
+    }
+
+    #[test]
+    fn fib_3() {
+        assert_eq!(fib(3), 2);
+    }
+}
