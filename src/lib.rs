3a008437750ac38e428edb948039cb1ff58d9994
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-09-14 07:49:05 +0200

    Warming up

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+pub fn fib(n: u32) -> u64 {
+    match n {
+        0 => panic!("Zero is not a valid argument"),
+        1 | 2 => 1,
+        _ => fib(n - 1) + fib(n - 2)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-
-    pub fn fib(n: u32) -> u64 {
-        match n {
-            0 => panic!("Zero is not a valid argument"),
-            1 | 2 => 1,
-            _ => fib(n - 1) + fib(n - 2)
-        }
-    }
- 
-    #[test]
+    use super::*;
+    
+     #[test]
     fn fib_1() {
         assert_eq!(fib(1), 1);
     }
